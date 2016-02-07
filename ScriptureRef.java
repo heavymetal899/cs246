2caabdaa723caed8d07cf40cfd15f41512b429da
heavymetal899 <heavymetal899@gmail.com>
2016-02-06 21:46:53 -0700

    My Scripture Reference

---
+++ ScriptureRef.java
@@ -0,0 +1,72 @@
+package com.example.steve.scriptureref;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class ScriptureRef extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.example.steve.scriptureref.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scripture_ref);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_scripture_ref, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void display (View v) {
+        Button button = (Button)v;
+        Intent intent = new Intent(this, RefDisplay.class);
+
+        EditText book = (EditText)findViewById(R.id.Book);
+        EditText chapter = (EditText)findViewById(R.id.Chapter);
+        EditText verse = (EditText)findViewById(R.id.Verse);
+
+        String reference = "Your favorite scripture is: " +book.getText().toString() + " "
+                + chapter.getText().toString() + ":" + verse.getText().toString();
+
+        intent.putExtra(EXTRA_MESSAGE, reference);
+        startActivity(intent);
+    }
+}
